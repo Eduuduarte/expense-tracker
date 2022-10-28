fc050244179289acb5bef4f6cc34d1435c1b758f
Eduardo Duarte <eduardoievolucao@gmail.com>
2022-10-27 21:43:24 -0300

    start bluild the table area

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
+import { useEffect, useState } from 'react';
 import * as C from './App.styles';
 
+import { Category } from './types/Category';
+import { Item } from './types/Item';
+
+import { categories } from './data/categories';
+import { items } from './data/items';
+
+import { getCurrentMonth, FilterListByMonth } from './helpers/dateFilter';
+import { TableArea } from './components/TableArea';
+
 const App = () => {
+  const [list, setList] = useState(items);
+  const [filteredList, setFilteredList] = useState<Item[]>([]);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+
+  useEffect(()=> {
+    setFilteredList(FilterListByMonth(list, currentMonth) );
+  }, [list, currentMonth])
+
   return( 
     <C.Container>
       <C.Header>
         <C.HeaderText>Sistema Financeiro</C.HeaderText>
       </C.Header>
       <C.Body>
-        ...
+        
+        {/* Área de informações */}
+
+        {/* Área de inserção */}
+
+        {/* Tabela de itens */}
+        <TableArea list={filteredList} />
+
       </C.Body>
     </C.Container>
   );
---
+++ src/components/TableItem/index.tsx
@@ -0,0 +1,14 @@
+import * as C from './style';
+import { Item } from '../../types/Item';
+
+type Props = {
+    item: Item
+}
+
+export const TableItem = ({item}: Props) => {
+    return(
+        <C.TableLine>
+
+        </C.TableLine>
+    );
+}
